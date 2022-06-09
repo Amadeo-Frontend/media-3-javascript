a8989327d5c589914a47c46055ac0a4f5cfeac7e
Amadeo Bon <amadeosph@outlook.com>
2022-06-09 13:09:00 -0300

    erro

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 function obterMedia() {
-    var c = parseInt(document.getElementById("c").value);
-    var a = parseInt(document.getElementById("a").value);
-    var b = parseInt(document.getElementById("b").value);
+  var a = parseInt(document.getElementById("a").value);
+  var b = parseInt(document.getElementById("b").value);
+  var c = parseInt(document.getElementById("c").value);
 
-    var media = (a + b + c) / 3;
 
-    document.getElementById("resultado").innerHTML = "Resultado: " + media;
+  var media = (a + b + c) / 3;
+  
+  document.getElementById("resultado").innerHTML = "Resultado: " + media;
+   if (a,b,c == "" || a,b,c == null) {
+    alert("Preencha todos os campos!");
+    return false;
   }
+ 
+}
+
+
+
