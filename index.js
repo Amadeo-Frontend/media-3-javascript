32f45cb5f8af3e1b79453ddc15beb748e0db2779
Amadeo Bon <amadeosph@outlook.com>
2022-06-14 02:36:04 -0300

    styles

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 function obterMedia() {
-  var a = parseInt(document.getElementById("a").value);
-  var b = parseInt(document.getElementById("b").value);
-  var c = parseInt(document.getElementById("c").value);
+  var a = document.getElementById("a").value;
+  var b = document.getElementById("b").value;
+  var c = document.getElementById("c").value;
 
-
-  var media = (a + b + c) / 3;
-  
-  document.getElementById("resultado").innerHTML = "Resultado: " + media;
-   if (a,b,c == "" || a,b,c == null) {
+  if (a === "" || b === "" || c === "") {
     alert("Preencha todos os campos!");
     return false;
   }
- 
+
+  if (a <= '0' || b <= '0' || c <= '0') {  
+    alert("Numero invalido!");
+    return false;
+  }
+  var media = (a + b + c) / 3;
+
+  document.getElementById("resultado").innerHTML = "Resultado: " + media;
 }
-
-
-
