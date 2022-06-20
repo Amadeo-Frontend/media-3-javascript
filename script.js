e68ec67d6dc726b4b6cf421fe26b5ea520bdb69d
Amadeo Bon <amadeosph@outlook.com>
2022-06-20 00:06:12 -0300

    exercício finalizado

--- script.js
+++ script.js
@@ -2,19 +2,17 @@ function obterMedia() {
   var a = document.getElementById("a").value;
   var b = document.getElementById("b").value;
   var c = document.getElementById("c").value;
-  
-    if (a === "" || b === "" || c === "") {
-      alert("Preencha todos os campos");
-      return false;
-    }
- 
 
-  if (a <= '0' || b <= '0' || c <= '0') {  
+  if (a === "" || b === "" || c === "") {
+    alert("Preencha todos os campos");
+    return false;
+  }
+
+  if (a < "0" || b < "0" || c < "0") {
     alert("Numero invalido!");
     return false;
   }
-  var media = (a + b + c) / 3;
+  var media = (parseInt(a) + parseInt(b) + parseInt(c)) / 3;
 
   document.getElementById("resultado").innerHTML = "Resultado: " + media;
 }
-
